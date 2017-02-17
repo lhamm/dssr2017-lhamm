e72e5dc68458903aa49e7cf0114c29ebf17c16e1
Lisa Hamm <l.hamm@auckland.ac.nz>
2017-02-17 15:10:03 +1300

    saves data files, asks for researcher, still needs eye analysis'

---
+++ dependencies/plotErrorTypes.m
@@ -0,0 +1,9 @@
+function labeledBar(xLabels, yValues)
+% first argument is a list of strings describing types or errors, but
+% nothing about correct
+% second argument is a list of proportions (between 0 and 1), which
+% includes correct proportion, and therefore should be one longer than the
+% length of possible errors
+
+bar(Proportion)
+
