47cd0aa0cb8b2eb1df6fc8e53debd8b258c6b078
Lisa Hamm <l.hamm@auckland.ac.nz>
2017-02-23 14:23:22 +1300

    testing code on all conditions

--- dependencies/AnalysisTesting/WhatIsLeftToDo.m
+++ dependencies/AnalysisTesting/WhatIsLeftToDo.m
@@ -3,7 +3,7 @@ function NewCombinationList          = WhatIsLeftToDo(AllCombinations,SumDUnique
 %% needs to be tested!
 %LISATODO: check what happens when this is done.
 
-for i=1:length(SumDUnique)
+for i=1:size(SumDUnique,1)
         ObsDone           = find(AllCombinations(:,1)==SumDUnique(i,1) & AllCombinations(:,2)==SumDUnique(i,2) & AllCombinations(:,3)==SumDUnique(i,3) & AllCombinations(:,4)==SumDUnique(i,4));   
         AllCombinations(ObsDone,:)=[];
 end
